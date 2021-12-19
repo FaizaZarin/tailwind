30c94b55d14cec0b39339e1bf3f8dae73a49b781
FaizaZarin <faizazania692@gmail.com>
2021-12-19 17:07:06 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors :{
+        pri: '#FF6363',
+        sec: {
+          100: '#E2E205',
+          200: '#888885',
+        }
+      },
+      fontfamily :{
+        body :['Poppins']
+      }
+  },
+  plugins: [],
+}
+}
